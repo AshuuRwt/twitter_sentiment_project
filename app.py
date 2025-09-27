a2add14b80a8fbc041fef05d54c234115d1bd662
Ayush Rawat <rawatayush9318@gmail.com>
2025-09-27 22:35:53 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle 
+import time
+
+st.title("Twitter Sentimental Analyses")
+
+model = pickle.load(open('twittwesentiment.pkl','rb'))
+
+tweet = st.text_input("Enter your tweet")
+
+submit =st.button('Predict')
+
+if submit:
+    start =time.time()
+    prediction = model.predict([tweet])
+    end=time.time()
+    st.write('Prediction time taken : ',round(end-start,2),'seconds')
+    print(prediction[0])
+    st.write("Prediction Sentiment is : ",prediction[0])
